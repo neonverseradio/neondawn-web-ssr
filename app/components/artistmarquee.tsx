049416074d03170747f8d62b2f7d286739415181
Maker <neonverseradio@gmail.com>
2026-08-08 09:23:30 -0400

    Update website files

--- app/components/artistmarquee.tsx
+++ app/components/artistmarquee.tsx
@@ -2,60 +2,7 @@ import { Link } from "react-router";
 import "../styles/artistmarquee.css";
 
 const artists = [
-  {
-    name: "Lynney",
-    caption: "Lost",
-    image: "/header/lynney1.jpg",
-    link: "/lynney",
-  },
-  {
-    name: "Martyrs",
-    caption: "Church Street EP ♫ Twist the Cap ♫ Drowning Days",
-    image: "/header/martyrs.png",
-    link: "/martyrs",
-  },
-  {
-    name: "Meeka",
-    caption: "Dissociate",
-    image: "/header/meeka.png",
-    link: "/meeka",
-  },
-  {
-    name: "Karl",
-    caption: "White Bat Audio♫Hackers",
-    image: "/header/karl.png",
-    link: "/karl",
-  },
-  {
-    name: "Ilovemakonnen - YTC",
-    caption: "Control",
-    image: "/header/ilovemakonnen.png",
-    link: "/mak",
-  },
-  {
-    name: "YTC - Ilovemakonnen",
-    caption: "Stuntin On Em 193 YTC Mak Radio Edit",
-    image: "/header/ytc.png",
-    link: "/ytc",
-  },
-  {
-    name: "Keshon Campbell",
-    caption: "Desires",
-    image: "/header/keshon.png",
-    link: "/keshon",
-  },
-{
-    name: "Novelley",
-    caption: "Lost Alley♫Time and Space",
-    image: "/header/novelley4.jpg",
-    link: "/novelley",
-  },
-  {
-    name: "Kurt",
-    caption: "When Angels Sing",
-    image: "/header/kurt2.png",
-    link: "/kurt",
-  },
+ 
 ];
 
 function ArtistTrack() {
